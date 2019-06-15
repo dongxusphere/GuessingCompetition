c8b965f0e1cb271394f8577e0ecabecaa4fa660a
zack098711 <zhaodongxvdlut@gmail.com>
2019-06-15 14:00:53 +0800

    游戏页面

--- Android/app/src/main/java/guess/competion/com/ui/game/GameFragment.java
+++ Android/app/src/main/java/guess/competion/com/ui/game/GameFragment.java
@@ -4,16 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import guess.competion.com.R;
-import guess.competion.com.ui.dashboard.DashboardViewModel;
+import guess.competion.data.DeviceTool;
 
 
 /**
@@ -24,19 +23,18 @@ import guess.competion.com.ui.dashboard.DashboardViewModel;
  */
 public class GameFragment extends Fragment {
     private GameViewModel gameViewModel;
-
+    private ImageView iv1;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         gameViewModel =
                 ViewModelProviders.of(this).get(GameViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        gameViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        View root = inflater.inflate(R.layout.fragment_game, container, false);
+        iv1= (ImageView )root.findViewById(R.id.iv1);
+        int width = DeviceTool.getScreenWidth(getContext());
+        int  height1 = (int) ((2320f/1080f)*width);
+        LinearLayout.LayoutParams l1 = new LinearLayout.LayoutParams(width,height1);
+        l1.setMargins(0,0,0,DeviceTool.dp2px(getContext(),60));
+        iv1.setLayoutParams(l1);
         return root;
     }
 }
